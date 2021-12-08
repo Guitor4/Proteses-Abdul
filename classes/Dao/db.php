db3d957c78f819ea4259840e521b047e5c4c3647
FernandoOlimpio <folimpio09@gmail.com>
2021-12-08 10:46:56 -0200

    faltando imagens

--- classes/Dao/db.php
+++ classes/Dao/db.php
@@ -30,7 +30,7 @@ class db
      * Senha do banco de dados
      * @var string
      */
-    const PASS = '';
+    const PASS = 'senac';
 
 
     /**
--- consultasAbrirProntuario.php
+++ consultasAbrirProntuario.php
@@ -10,12 +10,12 @@ use Classes\Dao\db;
 $prontuario = $_REQUEST['prontuario'];
 $_SESSION['prontuario'] = $prontuario;
 sleep(1);
-$query = "SELECT * from consulta inner join paciente "
+$query = "SELECT DISTINCT * from consulta inner join paciente "
     . "on fkProntuario=prontuario "
     . "inner join clinica on CFKClinica=idClinica "
     . "inner join dentista on CFKDentista=idDentista "
-    . "inner join tratamento on fkConsulta=idConsulta "
-    . "inner join procedimento on fkProcedimento=idProcedimento "
+//    . "inner join tratamento on fkConsulta=idConsulta "
+//    . "inner join procedimento on fkProcedimento=idProcedimento "
     . "where prontuario=" . $prontuario;
 if ($prontuario != null) {
     $prontuario1 = (new db())->executeSQL($query);
@@ -23,18 +23,23 @@ if ($prontuario != null) {
     $array = [];
     if ($prontuario1->rowCount() > 0) {
         while ($row_prontuario1 = $prontuario1->fetch(PDO::FETCH_ASSOC)) {
-            $array[] = array(
-           'id' => $row_prontuario1['idConsulta'],
+            
+            if (!in_array($row_prontuario1['idConsulta'], $array)) {
+                $array[] = array(
+                    'id' => $row_prontuario1['idConsulta'],
            'data' => date('d/m/y', strtotime($row_prontuario1['dataConsulta'])),
            'hora' => $row_prontuario1['horaConsulta'],
            'status' => $row_prontuario1['statusConsulta'],
            'clinica' => $row_prontuario1['nomeClinica'],
            'dentista' => $row_prontuario1['nomeDentista'],
-           'procedimento' => $row_prontuario1['nomeProcedimento'],
-           'idProcedimento' => $row_prontuario1['idProcedimento'],
+//           'procedimento' => $row_prontuario1['nomeProcedimento'],
+//           'idProcedimento' => $row_prontuario1['idProcedimento'],
            'prontuario' => $row_prontuario1['prontuario'],
+                    );
+            }
+           
          
-            );
+            
         }
         echo json_encode($array);
     }else{
